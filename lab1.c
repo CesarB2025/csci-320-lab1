f46b9594dd7141e84aafba65d20ac7e5c08bd454
cibautista21@students.desu.edu <cibautista21@students.desu.edu>
2023-10-08 23:48:31 +0000

    edited Makefile

--- lab1.c
+++ lab1.c
@@ -2,4 +2,31 @@
  * Implement your solution in thi file
  *
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab1.h"
+#include <string.h>
 
+char* readString(char* fileName){
+    char * myString;
+    myString = (char *) malloc(100);
+    FILE *fptr;
+    fptr = fopen(fileName, "r");
+    if(fptr == NULL){
+        return "Not able to open file\n";
+    }
+    fgets(myString, sizeof(myString), fptr);
+    fclose(fptr);
+    myString[strcspn(myString, "\n")] = '\0';
+    return myString;
+}
+
+char* mysteryExplode(const char* str){
+    int i;
+    int length = ((strlen(str)*(strlen(str) + 1))/ 2) +1;
+    char* answer = (char*)malloc(length);
+    memset(answer, '\0', length);
+    for(i=0; i <= strlen(str);i++)
+        strncat(answer,str,i);
+    return answer;
+}
